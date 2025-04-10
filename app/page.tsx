304d1f41018f3284cba89995d8f554556c801456
Ye Joo Park <subwaymatch@gmail.com>
2025-04-10 06:51:09 -0500

    Add simple query runner page

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
     <div>
       <div className="flex items-center justify-center h-screen bg-gray-100">
         <div className="flex flex-col gap-2 text-lg">
+          <Link href="/version/">Version</Link>
           <Link href="/basic">Basic</Link>
+          <Link href="/simple-query-runner">Simple Query Runner</Link>
           <Link href="/dashboard/">Dashboard</Link>
           <Link href="/query-editor/">Query Editor</Link>
-          <Link href="/version/">Version</Link>
         </div>
       </div>
     </div>
